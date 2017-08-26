6ed0680e45a3c3892611515682a7f0510c857114
patrycjakwiecien <patrycjaapril@gmail.com>
2017-08-26 12:34:37 +0200

    klasy i id

---
+++ js/selektory.js
@@ -0,0 +1,14 @@
+$(function)(){
+    //pobieranie elementow po znaczniku
+    var paragraphs = $('p');
+    console.log(paragraphs);
+    
+    //pobieranie elementow po id
+    var paragraphOne = $('#paragraf');
+    console.log(paragraphOne);
+    
+    //pobieranie elementow po klasie
+    var paragraphTwo = $('.paragraph')
+     console.log(paragraphTwo);
+    
+});
